95dec5eafa89da493a5fde8ae39eb088075aefd0
Ryan P <rdpinkstaff@gmail.com>
2023-03-09 16:05:52 -0700

    Working on styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import reactLogo from './assets/react.svg'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-center">
